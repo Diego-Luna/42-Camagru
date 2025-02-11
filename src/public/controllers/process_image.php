b72cf10d67eca36ce418b68ebad58911fcd8bcbb
Diego-Luna <diegofcolunalopez@gmail.com>
2025-02-11 13:59:39 +0100

    analises - 2

--- src/public/controllers/process_image.php
+++ src/public/controllers/process_image.php
@@ -3,8 +3,8 @@ require_once __DIR__ . '/../../controllers/sessionController.php';
 require_once __DIR__ . '/../../models/Image.php';
 require_once __DIR__ . '/../../config/database.php';
 
-// ini_set('display_errors', 0);
-// error_reporting(E_ALL);
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
 
 SessionController::requireLogin();
 
@@ -15,7 +15,7 @@ try {
         throw new Exception('Missing image or stickers');
     }
 
-    $uploadDir = __DIR__ '/uploads/';
+    $uploadDir = __DIR__ . '/uploads/';
     if (!file_exists($uploadDir)) {
         mkdir($uploadDir, 0777, true);
         if (!file_exists($uploadDir)) {
@@ -33,7 +33,7 @@ try {
     $success = Image::save($_SESSION['user_id'], $uploadFile);
     
     // Limpiar cualquier salida inesperada antes de emitir el JSON
-    // ob_clean();
+    ob_clean();
     echo json_encode([
         'success' => $success,
         'path' => $uploadFile
@@ -41,7 +41,7 @@ try {
 
 } catch (Exception $e) {
     error_log($e->getMessage());
-    // ob_clean();
+    ob_clean();
     echo json_encode([
         'success' => false,
         'error' => $e->getMessage()
